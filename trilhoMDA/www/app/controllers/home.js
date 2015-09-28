29e0f34cb2c54b1a0c92556b552efd97a584c4ca
Alan Thales <alanthales@gmail.com>
2015-09-28 20:21:04 -0300

    change tab colors on select

--- trilhoMDA/www/app/controllers/home.js
+++ trilhoMDA/www/app/controllers/home.js
@@ -1,4 +1,19 @@
 angular.module('controllers.home', ['ionic'])
 
-.controller('AppCtrl', function($scope) {
+.controller('AppCtrl', function($scope, $rootScope) {
+    $scope.$on('$stateChangeSuccess', function(event, toState) {
+        switch(toState.name) {
+            case 'app.contatos':
+                $scope.tabColor = 'assertive';
+                break;
+            case 'app.celula':
+                $scope.tabColor = 'positive';
+                break;
+            case 'app.chamada':
+                $scope.tabColor = 'balanced';
+                break;
+            default:
+                $scope.tabColor = 'energized';
+        }
+    });
 });
