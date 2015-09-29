54c5591e2bc5e96045a0e182c517ba6e0e1368e0
Alan Thales <alanthales@gmail.com>
2015-09-29 07:46:30 -0300

    various modifications

--- trilhoMDA/www/app/controllers/trilho.js
+++ trilhoMDA/www/app/controllers/trilho.js
@@ -10,8 +10,27 @@ angular.module('controllers.trilho', ['ionic'])
     
     $scope.atributos = [
         { id: 'attr1', descricao: 'Fez contato 24h' },
-        { id: 'attr2', descricao: 'Já foi integrado na célula' },
-        { id: 'attr3', descricao: 'Deu testemunho na célula' }
+        { id: 'attr2', descricao: 'Fez a 1° visita do acompanhamento inicial' },
+        { id: 'attr3', descricao: 'Já começou o discipulado inicial' },
+        { id: 'attr4', descricao: 'Foi integrado na célula' },
+        { id: 'attr5', descricao: 'Começou a frequentar o culto de celebração' },
+        { id: 'attr6', descricao: 'Conclui o acompanhamento inicial' },
+        { id: 'attr7', descricao: 'Fez o pré, encontro e pós-encontro' },
+        { id: 'attr8', descricao: 'Fez o curso de membresia' },
+        { id: 'attr9', descricao: 'Já fez entrevista pré-batismo' },
+        { id: 'attr10', descricao: 'Deu testemunho na célula' },
+        { id: 'attr11', descricao: 'Foi batizado nas águas' },
+        { id: 'attr12', descricao: 'Foi batizado no Espírito Santo e ora em línguas' },
+        { id: 'attr13', descricao: 'Está matriculado no CTL' },
+        { id: 'attr14', descricao: 'É fiel nos dízimos e ofertas' },
+        { id: 'attr15', descricao: 'Tem compromisso de ganhar a família pra Jesus' },
+        { id: 'attr16', descricao: 'Freqüenta fielmente o TADEL' },
+        { id: 'attr17', descricao: 'Já tem MDA-3' },
+        { id: 'attr18', descricao: 'Participou ativamente de multiplicação de célula' },
+        { id: 'attr19', descricao: 'Passou pelo preparo prático da célula' },
+        { id: 'attr20', descricao: 'É padrão dos fiéis' },
+        { id: 'attr21', descricao: 'Tem aprovação do pastor para ser líder de célula' },
+        { id: 'attr22', descricao: 'Completou o CTL' }
     ];
     
     $scope.countAttr = function(trilhoId) {
