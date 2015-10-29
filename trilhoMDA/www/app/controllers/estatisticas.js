212cf302ae2d8bec10e4c2feaf0a3e6cdab5fa1c
Alan Thales <alanthales@gmail.com>
2015-10-29 18:16:50 -0200

    bug fix

--- trilhoMDA/www/app/controllers/estatisticas.js
+++ trilhoMDA/www/app/controllers/estatisticas.js
@@ -20,51 +20,48 @@ angular.module('controllers.estatisticas', ['ionic'])
         }
 
         var i = 0, l = results.length,
-            membros = {}, //osmais = [], osmenos = [],
+            membros = new HashMap(), //osmais = [], osmenos = [],
             ofertas = 0, visitantes = 0,
-            j, obj;
-        
-//        for (j in $scope.celula.membros) {
-//            membros[j] = 0;
-//        }
+            j, obj, idx;
         
         for (; i < l; i++) {
-            ofertas += results[i].oferta;
-            visitantes += results[i].visitantes.length;
+            ofertas += parseFloat(results[i].oferta);
+            visitantes += parseInt(results[i].visitantes.length);
             
             for (j = 0; j < results[i].membros.length; j++) {
                 obj = results[i].membros[j];
-                if (obj.presente) {
-                    membros[obj.id] = membros[obj.id] ? membros[obj.id]+1 : 1;
+                idx = membros.indexOfKey('id', obj.id);
+                
+                if (idx === -1) {
+                    membros.put({ id: obj.id, peso: (obj.presente ? 1 : -1) });
                 } else {
-                    membros[obj.id] = membros[obj.id] ? membros[obj.id]-1 : -1;
+                    membros[idx].peso = obj.presente ? membros[idx].peso +1 : membros[idx].peso -1;
                 }
+                
+//                if (obj.presente) {
+//                    membros[obj.id] = membros[obj.id] ? membros[obj.id]+1 : 1;
+//                } else {
+//                    membros[obj.id] = membros[obj.id] ? membros[obj.id]-1 : -1;
+//                }
             }
         }
-        
-        console.log(ofertas, visitantes);
         
         $scope.estat.ofertas = ofertas / l;
         $scope.estat.visitantes = visitantes / l;
         
-        for (j in membros) {
-            if (membros[j] > 0) {
-                $scope.estat.osmais.push(j);
-            }
-            if (membros[j] < 0) {
-                $scope.estat.osmenos.push(j);
-            }
-        }
+//        for (j in membros) {
+//            if (membros[j] > 0) {
+//                $scope.estat.osmais.push(j);
+//            }
+//            if (membros[j] < 0) {
+//                $scope.estat.osmenos.push(j);
+//            }
+//        }
         
-        $scope.estat.osmais.sort(function(a, b){return b-a});
-        if ($scope.estat.osmais.length > 3) {
-            $scope.estat.osmais.slice(2);
-        }
+        membros.sort(function(a, b){return a.peso - b.peso});
         
-        $scope.estat.osmenos.sort(function(a, b){return b-a});
-        if ($scope.estat.osmenos.length > 3) {
-            $scope.estat.osmenos.slice(2);
-        }
+        $scope.estat.osmais = membros.slice(-3);
+        $scope.estat.osmenos = membros.slice(0, 3);
         
         console.log($scope.estat.osmais);
         console.log($scope.estat.osmenos);
