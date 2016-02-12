58c573a45a64ce7ea4b3c057baeeae8568b1adae
Alan Thales <alanthales@gmail.com>
2016-02-12 18:03:48 -0200

    ...

--- budget/www/app/controllers/dashboard.js
+++ budget/www/app/controllers/dashboard.js
@@ -1,3 +1,19 @@
 angular.module('budget.dashboard', [])
-.controller('DashboardCtrl', function($scope) {
+.controller('DashboardCtrl', function($scope, daoFactory) {
+    $scope.marcadores = daoFactory.getMarcadores();
+    
+//    daoFactory.getDB().sum('despesas', { sum: ['valor'], group: ['marcador'] }, { valor: { $gt: 0 } }, function(results) {
+//        
+//    });
+    
+    daoFactory.getDB().select('despesas', { }, function(results) {
+        var grouped = [],
+            marcadores = [],
+            l = results.length,
+            i = 0;
+        
+        for (; i < l; i++) {
+            
+        }
+    });
 });
