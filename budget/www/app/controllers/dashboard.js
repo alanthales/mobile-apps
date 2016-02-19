3ecd65836712d65829d0902deddb2defff9dd1b6
Alan Thales <alanthales@gmail.com>
2016-02-19 18:06:21 -0300

    init new modifications

--- budget/www/app/controllers/dashboard.js
+++ budget/www/app/controllers/dashboard.js
@@ -1,17 +1,19 @@
 angular.module('budget.dashboard', [])
 .controller('DashboardCtrl', function($scope, daoFactory) {
     $scope.hoje = new Date();
+    $scope.hoje = new Date();
 
     $scope.marcadores = daoFactory.getMarcadores();
     
     daoFactory.getDespesas(function(results) {
-        $scope.totalMes = results
-            .query({ ano: { $lte: $scope.hoje.getFullYear() }, mes: { $lte: $scope.hoje.getMonth() } })
+        $scope.mesAtual = results
+            .query({ ano: $scope.hoje.getFullYear(), mes: $scope.hoje.getMonth() })
+            .groupBy({ $sum: 'valor' }, ['ano', 'mes', 'marcadores'])
+            .orderBy({ ano: 'desc', mes: 'desc', valor: 'desc' });
+        
+        $scope.mesAnterior = results
+            .query({ ano: $scope.hoje.getFullYear(), mes: $scope.hoje.getMonth() })
             .groupBy({ $sum: 'valor' }, ['ano', 'mes', 'marcadores'])
             .orderBy({ ano: 'desc', mes: 'desc', valor: 'desc' });
     });
-    
-    $scope.saveIndex = function(index) {
-        $scope.myIndex = index;
-    };
 });
