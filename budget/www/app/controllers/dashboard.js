a1276e274a8538270268ea4f74ef09de1aeec175
Alan Thales <alanthales@gmail.com>
2016-02-22 09:21:39 -0300

    dashboard page working

--- budget/www/app/controllers/dashboard.js
+++ budget/www/app/controllers/dashboard.js
@@ -1,19 +1,36 @@
 angular.module('budget.dashboard', [])
 .controller('DashboardCtrl', function($scope, daoFactory) {
-    $scope.hoje = new Date();
-    $scope.hoje = new Date();
-
+    $scope.dtHoje = new Date();
+    $scope.dtAnterior = new Date();
+    
+    $scope.dtAnterior.setMonth($scope.dtHoje.getMonth() - 1);
+    
     $scope.marcadores = daoFactory.getMarcadores();
     
     daoFactory.getDespesas(function(results) {
         $scope.mesAtual = results
-            .query({ ano: $scope.hoje.getFullYear(), mes: $scope.hoje.getMonth() })
+            .query({ ano: $scope.dtHoje.getFullYear(), mes: $scope.dtHoje.getMonth() })
             .groupBy({ $sum: 'valor' }, ['ano', 'mes', 'marcadores'])
-            .orderBy({ ano: 'desc', mes: 'desc', valor: 'desc' });
+            .orderBy({ valor: 'desc' });
         
         $scope.mesAnterior = results
-            .query({ ano: $scope.hoje.getFullYear(), mes: $scope.hoje.getMonth() })
+            .query({ ano: $scope.dtAnterior.getFullYear(), mes: $scope.dtAnterior.getMonth() })
             .groupBy({ $sum: 'valor' }, ['ano', 'mes', 'marcadores'])
-            .orderBy({ ano: 'desc', mes: 'desc', valor: 'desc' });
+            .orderBy({ valor: 'desc' });
     });
+    
+    $scope.getClass = function(item) {
+        console.log('getClass');
+        var resultado = $scope.mesAnterior.query({ marcadores: { $contain: item.marcadores } });
+        if (!resultado.length) {
+            return 'positive';
+        }
+        if (item.valor > resultado[0].valor) {
+            return 'assertive';
+        }
+        if (item.valor < resultado[0].valor) {
+            return 'balanced';
+        }
+        return ''
+    }
 });
