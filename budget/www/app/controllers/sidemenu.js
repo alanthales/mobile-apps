ebcb57c84fb0d6cbc40fbf4365f317828eff7d8f
Matheus Barbosa <matheus.sob@gmail.com>
2016-02-29 01:20:37 -0300

    added submenu to ion-side-menu

--- budget/www/app/controllers/sidemenu.js
+++ budget/www/app/controllers/sidemenu.js
@@ -1,4 +1,16 @@
 angular.module('budget.sidemenu', [])
 .controller('SideMenuCtrl', function($scope, daoFactory) {
     $scope.marcadores = daoFactory.getMarcadores();
+    $scope.selections = [];
+    
+    $scope.openSub = function(evt, key) {
+        $scope.selections[$scope.selections.length] = {
+            key: key, 
+            title: evt.currentTarget.getElementsByClassName('title')[0].textContent
+        };
+    }
+    $scope.backToBefore = function() {
+        $scope.selections.pop();
+        $scope.submenu = $scope.selections.length > 1;
+    }    
 });
