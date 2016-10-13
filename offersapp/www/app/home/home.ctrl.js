1d78fcf6642d30991dea53fe17624d80d6eeef50
Alan Thales <alanthales@gmail.com>
2016-10-13 01:19:58 -0300

    trying different layout

--- offersapp/www/app/home/home.ctrl.js
+++ offersapp/www/app/home/home.ctrl.js
@@ -2,12 +2,14 @@ angular.module('offersapp.home', [])
 .controller('HomeCtrl', function($scope, $timeout, ionicMaterialInk, ionicMaterialMotion, DaoFact){
     this.ofertas = DaoFact.getOfertas();
     
+//    this.gridLenght = { 'height': Math.ceil(this.ofertas.data.length / 4) * 150 + 'vw' };
+        
     // Activate ink for controller
     ionicMaterialInk.displayEffect();
     
     $timeout(function() {
         ionicMaterialMotion.fadeSlideInRight({
-            selector: '.animate-fade-slide-in .card'
+            selector: '.animate-fade-slide-in .item'
         });
     }, 300);
 });
