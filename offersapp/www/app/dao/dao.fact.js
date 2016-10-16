ee6bad7c9fc7d391b53d592fdcc8d35d107de309
Alan Thales <alanthales@gmail.com>
2016-10-16 14:04:42 -0200

    change layout

--- offersapp/www/app/dao/dao.fact.js
+++ offersapp/www/app/dao/dao.fact.js
@@ -5,9 +5,9 @@ angular.module('offersapp.dao', [])
         ofertas = new SimpleDataSet();
     
     anunciantes.insertAll([
-        { id: 1, razao: 'Adicao Comércio' },
-        { id: 2, razao: 'Mercearia do Xico' },
-        { id: 3, razao: 'Açougue do João' }
+        { id: 1, razao: 'Adicao Comércio', endereco: 'Av. São João', numero: '380', cidade: 'Campo Belo', estado: 'MG' },
+        { id: 2, razao: 'Mercearia do Xico', endereco: 'Rua Francisco Gibram', numero: '661', cidade: 'Campo Belo', estado: 'MG' },
+        { id: 3, razao: 'Açougue do João', endereco: 'Rua Floriano Peixoto', numero: '201', cidade: 'Campo Belo', estado: 'MG' }
     ]);
     
     categorias.insertAll([
@@ -20,11 +20,11 @@ angular.module('offersapp.dao', [])
     ofertas.insertAll([
         { id: 1, descricao: "Arroz D'Mata Tipo 1 PCT 5kg", valor: 14.29, validade: new Date(), anunciante: anunciantes.getById(1), imagem: './img/arroz pct.jpg', categoriaId: 4 },
         { id: 2, descricao: "Feijão Galo PCT 3kg", valor: 10.49, validade: new Date(), anunciante: anunciantes.getById(1), imagem: './img/feijao pct.jpg', categoriaId: 4 },
-        { id: 3, descricao: "Extrato de Tomante Pomarola UN 350g", valor: 1.99, validade: new Date(), anunciante: anunciantes.getById(1), categoriaId: 3 },
+        { id: 3, descricao: "Extrato de Tomate Pomarola UN 350g", valor: 1.99, validade: new Date(), anunciante: anunciantes.getById(1), categoriaId: 3 },
         { id: 4, descricao: "Feijão da Roça Agranel KG", valor: 4.49, validade: new Date(), anunciante: anunciantes.getById(2), categoriaId: 4 },
         { id: 5, descricao: "Milho verde Fuggini LT 300g", valor: 1.29, validade: new Date(), anunciante: anunciantes.getById(2), categoriaId: 3 },
         { id: 6, descricao: "Azeitona Del Chef PT 800g", valor: 8.39, validade: new Date(), anunciante: anunciantes.getById(1), categoriaId: 3 },
-        { id: 7, descricao: "Carne Picanha Friboi KG", valor: 23.49, validade: new Date(), anunciante: anunciantes.getById(1), imagem: './img/carne friboi.png', categoriaId: 1 },
+        { id: 7, descricao: "Carne Picanha Friboi KG", valor: 23.49, validade: new Date(), anunciante: anunciantes.getById(1), imagem: './img/carne friboi.jpg', categoriaId: 1 },
         { id: 8, descricao: "Carne de porco Pernil KG", valor: 14.00, validade: new Date(), anunciante: anunciantes.getById(3), categoriaId: 1 }
     ]);
     
--- offersapp/www/app/offers/detail.ctrl.js
+++ offersapp/www/app/offers/detail.ctrl.js
@@ -3,10 +3,46 @@ angular.module('offersapp.offersdetail', [])
     var ofertaId = $stateParams.id ? parseInt($stateParams.id) : 0;
     
     this.oferta = DaoFact.getOfertas().getById(ofertaId);
-    
-    ionicMaterialMotion.slideUp({ selector: '.slide-up' });
-    
+        
     ionicMaterialMotion.fadeSlideInRight();
     
     ionicMaterialInk.displayEffect();
+    
+    var anunciante = this.oferta.anunciante,
+        endereco = [anunciante.endereco, ',', anunciante.numero, '-', anunciante.cidade, ' ', anunciante.estado].join(' '),
+        geocoder = new google.maps.Geocoder(),
+        mapOptions = {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        },
+        map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    
+    geocoder.geocode({ 'address': endereco }, function(results, status) {
+        if (status != google.maps.GeocoderStatus.OK || status == google.maps.GeocoderStatus.ZERO_RESULTS) {
+            return;
+        }
+        
+        map.setCenter(results[0].geometry.location);
+        
+        var infowindow = new google.maps.InfoWindow({
+                content: [
+                    '<b>',
+                        anunciante.razao,
+                    '</b>',
+                    '<br/>',
+                    endereco
+                ].join(''),
+                size: new google.maps.Size(150,50)
+            }),
+            marker = new google.maps.Marker({
+                position: results[0].geometry.location,
+                map: map,
+                title: anunciante.razao
+            });
+        
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map, marker);
+        });
+    });
 });
