38fa09cb24c093fb5ed5f24bf07d0392eb30cbd3
Alan Thales <alanthales@gmail.com>
2016-10-18 18:27:01 -0200

    added performance boost

--- offersapp/www/js/app.js
+++ offersapp/www/js/app.js
@@ -33,12 +33,9 @@ angular.module('offersapp', [
     });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($ionicConfigProvider, $stateProvider, $urlRouterProvider) {
+  $ionicConfigProvider.scrolling.jsScrolling(false);
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
 
   .state('app', {
