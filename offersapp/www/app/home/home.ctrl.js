e41c425fc37fb2ea31c8bfcb2b97aa3c8dd21f70
Alan Thales <alanthales@gmail.com>
2016-10-28 16:59:47 -0200

    fixed issues

--- offersapp/www/app/home/home.ctrl.js
+++ offersapp/www/app/home/home.ctrl.js
@@ -1,10 +1,12 @@
 angular.module('offersapp.home', [])
 .controller('HomeCtrl', function($scope, $timeout, DaoFact){
     this.ofertas = DaoFact.getOfertas();
+
+    this.loadMore = function() {
+//        this.ofertas.next();
+    };
     
     $timeout(function() {
         document.getElementsByClassName('button-fab-top-right')[0].classList.toggle('on');
     }, 300);
-    
-//    this.gridLenght = { 'height': Math.ceil(this.ofertas.data.length / 4) * 150 + 'vw' };
 });
--- offersapp/www/app/offers/detail.ctrl.js
+++ offersapp/www/app/offers/detail.ctrl.js
@@ -1,7 +1,9 @@
 angular.module('offersapp.offersdetail', [])
-.controller('OffersDetailCtrl', function($scope, $stateParams, $timeout, DaoFact, PopupServ, ionicMaterialInk, ionicMaterialMotion) {
-    var ofertaId = $stateParams.id ? parseInt($stateParams.id) : 0;
+.controller('OffersDetailCtrl', function($scope, $stateParams, $timeout, $ionicLoading, DaoFact, ionicMaterialInk, ionicMaterialMotion) {
+    var self = this,
+        ofertaId = $stateParams.id ? parseInt($stateParams.id) : 0;
     
+    this.tabIndex = 0;
     this.oferta = DaoFact.getOfertas().getById(ofertaId);
     
     this.add = function(oferta) {
@@ -12,47 +14,60 @@ angular.module('offersapp.offersdetail', [])
         lista.post();
         
         if (l < lista.data.length) {
-            PopupServ.alert('Oferta adicionada na lista de compra!');
+            $ionicLoading.show({
+                template: 'Oferta adicionada na lista',
+                noBackdrop: true,
+                duration: 2000
+            });
         }
     };
-
-    var anunciante = this.oferta.anunciante,
-        endereco = [anunciante.endereco, ',', anunciante.numero, '-', anunciante.cidade, ' ', anunciante.estado].join(' '),
-        geocoder = new google.maps.Geocoder(),
-        mapOptions = {
-            center: {lat: -34.397, lng: 150.644},
-            zoom: 16,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        },
-        map = new google.maps.Map(document.getElementById('map'), mapOptions);
     
-    geocoder.geocode({ 'address': endereco }, function(results, status) {
-        if (status != google.maps.GeocoderStatus.OK || status == google.maps.GeocoderStatus.ZERO_RESULTS) {
-            return;
+    this.changeTab = function(index) {
+        self.tabIndex = index;
+        if (index === 1) {
+            self.loadMap();
         }
-        
-        map.setCenter(results[0].geometry.location);
-        
-        var infowindow = new google.maps.InfoWindow({
-                content: [
-                    '<b>',
-                        anunciante.razao,
-                    '</b>',
-                    '<br/>',
-                    endereco
-                ].join(''),
-                size: new google.maps.Size(150,50)
-            }),
-            marker = new google.maps.Marker({
-                position: results[0].geometry.location,
-                map: map,
-                title: anunciante.razao
+    };
+    
+    this.loadMap = function() {
+        var anunciante = self.oferta.anunciante,
+            endereco = [anunciante.endereco, ',', anunciante.numero, '-', anunciante.cidade, ' ', anunciante.estado].join(' '),
+            geocoder = new google.maps.Geocoder(),
+            mapOptions = {
+                center: {lat: -34.397, lng: 150.644},
+                zoom: 16,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            },
+            map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+        geocoder.geocode({ 'address': endereco }, function(results, status) {
+            if (status != google.maps.GeocoderStatus.OK || status == google.maps.GeocoderStatus.ZERO_RESULTS) {
+                return;
+            }
+
+            map.setCenter(results[0].geometry.location);
+
+            var infowindow = new google.maps.InfoWindow({
+                    content: [
+                        '<b>',
+                            anunciante.razao,
+                        '</b>',
+                        '<br/>',
+                        endereco
+                    ].join(''),
+                    size: new google.maps.Size(150,50)
+                }),
+                marker = new google.maps.Marker({
+                    position: results[0].geometry.location,
+                    map: map,
+                    title: anunciante.razao
+                });
+
+            google.maps.event.addListener(marker, 'click', function() {
+                infowindow.open(map, marker);
             });
-        
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.open(map, marker);
         });
-    });
+    };
     
     ionicMaterialInk.displayEffect();
     
